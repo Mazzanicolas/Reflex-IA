437a8610c304c7a499563b0657a8b5c67abcfee1
Nicolas Mazza <mazzanicolas@gmail.com>
2018-11-25 17:54:24 -0300

    Item detection now working

--- reflexia.py
+++ reflexia.py
@@ -13,31 +13,33 @@ class Reflexia():
         """ 
         self.window_position = win32gui.FindWindow(None, window_name)
         self.items_on_board = {
-                               'star':  (1,'./items/star.png'),   # ID: 1
-                               'orb':   (2,'./items/orb.png'),    # ID: 2
-                               'potion':(3,'./items/potion.png'), # ID: 3
-                               'sword': (4,'./items/sword.png')   # ID: 4
+                               1: (1, 'STR','./items/star.png'),   # ID: 1
+                               2: (2, 'ORB','./items/orb.png'),    # ID: 2
+                               3: (3, 'PTN','./items/potion.png'), # ID: 3
+                               4: (4, 'SWD','./items/sword.png'),  # ID: 4
+                               5: (5, 'DMD','./items/diamond.png') # ID: 5
                               }
         self.turns_dimensions = {
                                  'top_corner':    (42, 18),
                                  'bottom_corner': (130, 38)
                                 }
         self.board_dimensions = {
-                                 'top_corner':    (233, 140),
+                                 'top_corner':    (233, 144),
                                  'bottom_corner': (708, 557),
                                 }
         self.health_dimensions = {
                                  'top_corner':    (38, 671),
                                  'bottom_corner': (87, 690)
                                 }
-        self.board_positions = np.array([[265-233, 175-140], [325-233, 175-140], [380-233, 175-140], [445-233, 175-140], [500-233, 175-140], [560-233, 175-140], [620-233, 175-140], [680-233, 175-140],
-                                [265-233, 235-140], [325-233, 235-140], [380-233, 235-140], [445-233, 235-140], [500-233, 235-140], [560-233, 235-140], [620-233, 235-140], [680-233, 235-140],
-                                [265-233, 290-140], [325-233, 290-140], [380-233, 290-140], [445-233, 290-140], [500-233, 290-140], [560-233, 290-140], [620-233, 290-140], [680-233, 290-140],
-                                [265-233, 350-140], [325-233, 350-140], [380-233, 350-140], [445-233, 350-140], [500-233, 350-140], [560-233, 350-140], [620-233, 350-140], [680-233, 350-140],
-                                [265-233, 410-140], [325-233, 410-140], [380-233, 410-140], [445-233, 410-140], [500-233, 410-140], [500-233, 410-140], [620-233, 410-140], [680-233, 410-140],
-                                [265-233, 470-140], [325-233, 470-140], [380-233, 470-140], [445-233, 470-140], [500-233, 470-140], [500-233, 470-140], [620-233, 470-140], [680-233, 470-140],
-                                [265-233, 530-140], [325-233, 530-140], [380-233, 530-140], [445-233, 530-140], [500-233, 530-140], [500-233, 530-140], [620-233, 530-140], [680-233, 530-140]])
-        self.board = np.zeros(7*8)
+        self.board_positions = np.array([[265, 175], [325, 175], [380, 175], [445, 175], [500, 175], [560, 175], [620, 175], [680, 175],
+                                         [265, 235], [325, 235], [380, 235], [445, 235], [500, 235], [560, 235], [620, 235], [680, 235],
+                                         [265, 290], [325, 290], [380, 290], [445, 290], [500, 290], [560, 290], [620, 290], [680, 290],
+                                         [265, 350], [325, 350], [380, 350], [445, 350], [500, 350], [560, 350], [620, 350], [680, 350],
+                                         [265, 410], [325, 410], [380, 410], [445, 410], [500, 410], [560, 410], [620, 410], [680, 410],
+                                         [265, 470], [325, 470], [380, 470], [445, 470], [500, 470], [560, 470], [620, 470], [680, 470],
+                                         [265, 530], [325, 530], [380, 530], [445, 530], [500, 530], [560, 530], [620, 530], [680, 530]])
+        # self.board = np.zeros(7*8)
+        self.board = np.zeros((8,7))
 
     def capture_screen(self):
         while True:
@@ -70,28 +72,15 @@ class Reflexia():
             ################### Get items on board ##################
             #########################################################
             x, y = self.board_dimensions['top_corner']
-            ###################### Find Potions #####################
-            potion_w, potion_h, potion_locations = self.detect_item(game_board_frame, 'potion', threshold=0.75)
-            game_frame = self.locate_on_board(game_frame, potion_locations, (0,255,0), 1)
-            ###################### Find Swords ######################
-            sword_w, sword_h, sword_locations = self.detect_item(game_board_frame, 'sword', threshold=0.75)
-            game_frame = self.locate_on_board(game_frame, sword_locations, (255,255,0), 1)
-            ####################### Find Stars ######################
-            star_w, star_h, star_locations = self.detect_item(game_board_frame, 'star', threshold=0.75)
-            game_frame = self.locate_on_board(game_frame, star_locations, (0,255,255), 1)
-            ######################## Find Orbs ######################
-            orb_w, orb_h, orb_locations = self.detect_item(game_board_frame, 'orb', threshold=0.75)
-            game_frame = self.locate_on_board(game_frame, orb_locations, (255,0,255), 1)
+            self.detect_items_on_board(game_board_frame)
             #########################################################
             ################# Reflexia Interpretation ###############
             #########################################################
-            cv2.putText(game_frame, 'Potions: '+str(len(potion_locations[0])),(50, 160),cv2.FONT_HERSHEY_PLAIN,1.0, (255,255,255), thickness=1)
-            # cv2.putText(game_frame, 'Swords: '+str(len(sword_locations[0])),(50, 180),cv2.FONT_HERSHEY_PLAIN,1.0, (255,255,255), thickness=1)
-            # cv2.putText(game_frame, 'Stars: '+str(len(star_locations[0])),(50, 200),cv2.FONT_HERSHEY_PLAIN,1.0, (255,255,255), thickness=1)
-            # cv2.putText(game_frame, 'Orbs: '+str(len(orb_locations[0])),(50, 220),cv2.FONT_HERSHEY_PLAIN,1.0, (255,255,255), thickness=1)
+            game_frame = self.locate_items_on_board(game_frame)
             #########################################################
             ################### Show window Frames ##################
             #########################################################
+            
             game_frame = cv2.cvtColor(game_frame, cv2.COLOR_BGR2RGB)
             cv2.imshow('Reflexia', game_frame)
             if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -107,81 +96,51 @@ class Reflexia():
         cv2.rectangle(game_board_frame, top_corner, bottom_corner, color, thickness)
         return game_board_frame
 
-    def locate_on_board(self, game_frame, locations, color=(0,255,0), thickness=10):
-        """
-        Draw a circle with the given dimensions, color & thickness over the given frame
-        """
-        x, y = self.board_dimensions['top_corner']
-        mask = np.zeros(game_frame.shape[:2], np.uint8)
-        prev_point = (-100, -100)
-        for point in zip(*locations[::-1]):
-            # cv2.putText(game_frame, str(prev_point[0]-18+25+x)+','+str(prev_point[1]-18+25+y),(prev_point[0]-18+25+x, prev_point[1]-18+25+y),cv2.FONT_HERSHEY_PLAIN,0.8, (255,255,255), thickness=1)
-            # cv2.putText(game_frame, str(prev_point[0]+18+25+x)+','+str(prev_point[1]+18+25+y),(prev_point[0]+18+25+x, prev_point[1]+18+25+y),cv2.FONT_HERSHEY_PLAIN,0.8, (255,255,255), thickness=1)
-            # if prev_point[0]-25 < point[0] and point[0] < prev_point[0]+25:
-                # prev_point = point
-            #     continue
-            # if prev_point[1]-25 < point[1] and point[1] < prev_point[1]+25:
-                # prev_point = point
-            #     continue
-            # prev_point = point
-            cv2.circle(game_frame, (point[0]+25+x, point[1]+25+y), 10, color, thickness)
-            # cv2.putText(game_frame, str(point[0]+25+x)+','+str(point[1]+25+y),(point[0]+25+x, point[1]+25+y),cv2.FONT_HERSHEY_PLAIN,0.8, (255,255,255), thickness=1)
-        return game_frame
-
-    def detect_item(self, game_frame, item_name, threshold=0.8):
+    def detect_items_on_board(self, game_board_frame):
         """
         Returns height, width of the maching template & location of the boxes
         """
-        img_gray = cv2.cvtColor(game_frame, cv2.COLOR_BGR2GRAY)
-        iD, item_path = self.items_on_board[item_name]
-        template = cv2.imread(item_path, 0)
-        width, height = template.shape[::-1]
-        maching_locations = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-        locations = np.where(maching_locations >= threshold)
-        # Remove next Duplicates TODO: Remove all duplicates
-        # > > > > > > > > > > > > > > > > > > > > > > > > > >
-        loc_0, loc_1 = locations
-        clean_loc_0  = []
-        clean_loc_1  = []
-        prev_point = [-100, -100]
-        for point in zip(*locations[::-1]):
-            if (prev_point[0]-10 < point[0] and point[0] < prev_point[0]+10) and \
-               (prev_point[1]-10 < point[1] and point[1] < prev_point[1]+10):
-                prev_point = point
-                continue
-            clean_loc_0.append(point[1])
-            clean_loc_1.append(point[0])
-            prev_point = point
-        locations = (np.array(clean_loc_0), np.array(clean_loc_1))
-        # < < < < < < < < < < < < < < < < < < < < < < < < < <
-        self.set_positions_on_board(locations, iD)
-        return width, height, locations
+        top_corner_x, top_corner_y = 0, 0
+        SQUARE_SIDE_LENGTH = 59
+        for row in range(1,9):
+            for column in range(1,8):
+                bottom_corner_x, bottom_corner_y = top_corner_x + SQUARE_SIDE_LENGTH, \
+                                                   top_corner_y + SQUARE_SIDE_LENGTH
+                item = self.get_most_similar_item(game_board_frame,
+                                                    top_corner_x,
+                                                    top_corner_y,
+                                                    bottom_corner_x,
+                                                    bottom_corner_y)
+                self.board[row-1][column-1] = item
+                top_corner_x += SQUARE_SIDE_LENGTH
+            top_corner_x = 0
+            top_corner_y = row*SQUARE_SIDE_LENGTH
 
-    def set_positions_on_board(self, locations, iD):
-    
-        unique_points = self.get_position_on_board(locations)
-        np.put(self.board, unique_points, iD)
-        print(self.board.reshape((7,8)))
+    def locate_items_on_board(self, game_frame):
+        for index, position in enumerate(self.board_positions):
+            item_id = self.board.T.flatten()[index]
+            try:
+                iD, item_name, path = self.items_on_board[item_id]
+            except:
+                item_name = 'UNK'
+            cv2.putText(game_frame, item_name,(position[0]-15,position[1]+2),cv2.FONT_HERSHEY_PLAIN,0.8, (255,255,255), thickness=2)
+            cv2.putText(game_frame, item_name,(position[0]-15,position[1]+2),cv2.FONT_HERSHEY_PLAIN,0.8, (255,0,0), thickness=1)
+        return game_frame
 
-    def get_position_on_board(self, locations):
-        distances = []
-        positions = []
-        # TODO: Use numpy broadcasting 
-        for point_found in zip(*locations[::-1]):
-            for point_on_board in self.board_positions:
-                distance = self.euclidean_distance(point_on_board, point_found)
-                distances.append(distance)
-            positions.append(np.argmin(np.array(distances)))
-            distances = []
-        unique_points = np.unique(positions)
-
-        return unique_points
-            # distances.append(np.argmin(self.euclidean_distance(point)))
-        # print(distances)
-        # return 
-    
-    def euclidean_distance(self, point_1, point_2):
-        return np.sqrt(np.sum((np.array([point_2[1], point_2[0]]) - np.array([point_1[1], point_1[0]]))**2))
+    def get_most_similar_item(self, game_board_frame, top_corner_x, top_corner_y, bottom_corner_x, bottom_corner_y):
+        cropped_item =  game_board_frame[top_corner_x:bottom_corner_x, top_corner_y:bottom_corner_y]
+        img_gray = cv2.cvtColor(cropped_item, cv2.COLOR_BGR2GRAY)
+        for item in self.items_on_board:
+            iD, name, item_path = self.items_on_board[item]
+            template = cv2.imread(item_path, 0)
+            width, height = template.shape[::-1]
+            maching_locations = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+            locations = np.where(maching_locations >= 0.6)
+            _, _ = locations
+            if len(_):
+                return iD
+        return -1
+        
 
 if __name__ == '__main__':
     print('> > > Start')
